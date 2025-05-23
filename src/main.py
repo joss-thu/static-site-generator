cec06ee296bd6203f73455f5cdddd293f3ddac9d
joss-thu <antony@mail.hs-ulm.de>
2025-05-23 12:56:29 +0200

    static folder with assets created.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
-from src.textnode import TextNode, TextType
-def main():
-    print(TextNode('This is some anchor text',TextType.LINK.value, 'https://www.boot.de'))
+import os
 
-# main()
+dest_path = '../public/'
+
+if os.path.exists(dest_path):
+    pass
+else:
+    pass
