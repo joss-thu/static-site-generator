d1e49e7afb1c6ba84bc32b77d0a9f20084b7aecf
joss-thu <antony@mail.hs-ulm.de>
2025-05-29 23:56:10 +0200

    build.sh updates

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 # build for deploy
 # This script is called in the deploy-site.yml
 
-python3 -m src.main "/static-site-generator/" # While deploying via github pages
-python3 -m src.main "/" # While deploying via netlify/vercel pages
+python3 -m src.main "/static-site-generator/"
